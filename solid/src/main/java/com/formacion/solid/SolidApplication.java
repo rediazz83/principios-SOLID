db599322ccb081f527d255c39e7ac30acc3d79fd
Rodrigo Diaz <esteban.diaz@globant.com>
2018-11-03 21:33:49 -0300

    Archivos fuente desafio propuesto para el principio de SINGLE RESPONSABILITY (no resuelto aun)

---
+++ solid/src/main/java/com/formacion/solid/SolidApplication.java
@@ -0,0 +1,12 @@
+package com.formacion.solid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidApplication.class, args);
+	}
+}
